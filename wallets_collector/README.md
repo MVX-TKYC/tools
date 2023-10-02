9575029fa0376259d11acbbfab5fdb7cd84974a0
Theo F <theo.farnole@gmail.com>
2023-10-02 17:49:11 +0200

    add parallelism + tqdm

--- wallets_collector/README.md
+++ wallets_collector/README.md
@@ -4,6 +4,8 @@ Query every transactions for each wallet and store them in /wallets folder
 
 # How to use 
 
+Create a txt containing all your wallets, then run
+
 ```
-py main.py
+py main.py path_to_txt
 ```
--- wallets_collector/main.py
+++ wallets_collector/main.py
@@ -1,16 +1,18 @@
 import os
 import json
-import time
 import requests
+import argparse
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
-# Create a 'wallets' directory if it does not exist
-os.makedirs("wallets", exist_ok=True)
-
-# Define the header for HTTP requests
-headers = {"Content-Type": "application/json"}
+WALLETS_FOLDER = "wallets"
+MAX_WORKERS = min(32, os.cpu_count() + 4)
 
 
 def get_request_content(url, query):
+    headers = {"Content-Type": "application/json"}
+
     # Send a POST request to the provided URL with the given query
     # Return the JSON content of the response
     return requests.post(url, headers=headers, data=json.dumps(query)).json()
@@ -23,30 +25,46 @@ def get_request_content_scroll(url, query):
     total_data = data["hits"]["total"]["value"]
 
     if 100000 < total_data:
-        print(f"Too much transactions ? ({total_data})")
+        tqdm.write(f"Too much transactions ? ({total_data})")
         return
     else:
         scroll_id = data["_scroll_id"]
         all_data = data["hits"]["hits"]
 
-        # Keep scrolling and collecting the data until we have collected all hits
-        while len(all_data) < total_data:
-            query = {"scroll": "1m", "scroll_id": scroll_id}
-            data = get_request_content(
-                "https://index.multiversx.com/_search/scroll", query)
-            scroll_id = data["_scroll_id"]
-            all_data += data["hits"]["hits"]
-        return all_data
+    # Keep scrolling and collecting the data until we have collected all hits
+    while len(all_data) < total_data:
+        query = {"scroll": "1m", "scroll_id": scroll_id}
+        data = get_request_content(
+            "https://index.multiversx.com/_search/scroll", query)
+        scroll_id = data["_scroll_id"]
+        all_data += data["hits"]["hits"]
+    return all_data
 
 
-# Start by querying the accounts
-url = "https://index.multiversx.com/accounts/_search?scroll=1m&size=10"
-query = {"query": {"match_all": {}}, "sort": [
-    {"balanceNum": {"order": "desc"}}], "track_total_hits": True}
-accounts = [e["_id"] for e in get_request_content(url, query)["hits"]["hits"]]
+def getAllWallets(file):
+    tqdm.write(f"Reading {file}")
 
-# Loop over each account and fetch the related transactions, smart contract results, and logs
-for i, wallet in enumerate(accounts):
+    def getFilesWithoutExtension(parent):
+        files = os.listdir(parent)
+
+        return [os.path.splitext(
+            filename)[0] for filename in files]
+
+    with open(file, "r") as accountsFile:
+        ignoredAccounts = getFilesWithoutExtension(WALLETS_FOLDER)
+
+        accounts = accountsFile.read().splitlines()
+
+        print("Found {0}/{1} ({2}%) accounts already processed.".format(
+            len(ignoredAccounts), len(accounts), round(len(ignoredAccounts) / len(accounts) * 100)))
+
+        accounts = filter(lambda a: not a.startswith(
+            "erd1qqq") and not a in ignoredAccounts, accounts)
+
+        return accounts
+
+
+def processWallet(wallet):
 
     url = "https://index.multiversx.com/transactions/_search?scroll=1m&size=10000"
     query = {"query": {"bool": {"should": [{"match": {e: wallet}} for e in [
@@ -54,7 +72,9 @@ for i, wallet in enumerate(accounts):
     data = get_request_content_scroll(url, query)
 
     if not data:
-        continue
+        with open(os.path.join(WALLETS_FOLDER, wallet+".txt"), "w") as f:
+            f.write("ignored")
+        return
 
     # Filter out the ids of transactions with results, operations, or logs
     ids = [e["_id"] for e in data if e["_source"].get(
@@ -98,5 +118,30 @@ for i, wallet in enumerate(accounts):
             data[i]["_source"].setdefault("events", []).extend(log)
 
     # Save the fetched and combined data for each wallet to a separate JSON file
-    with open(f"wallets/{wallet}.json", "w") as f:
+    with open(os.path.join(WALLETS_FOLDER, wallet+".json"), "w") as f:
         json.dump(data, f, ensure_ascii=False)
+
+
+def main():
+    # Create a 'wallets' directory if it does not exist
+    os.makedirs(WALLETS_FOLDER, exist_ok=True)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--list", help="The wallets list",
+                        type=str, default=r"lists\all_wallets.txt")
+    args = parser.parse_args()
+    pbar = tqdm(unit=" account")
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+
+        futures = []
+
+        for wallet in getAllWallets(args.list):
+            futures.append(executor.submit(processWallet, wallet))
+
+        for future in tqdm(concurrent.futures.as_completed(futures)):
+            pbar.update()
+
+
+if __name__ == "__main__":
+    main()
