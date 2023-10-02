8fc2d7cc1bc8c0983809a2f6777eb28e078e1175
Theo F <theo.farnole@gmail.com>
2023-10-02 18:25:39 +0200

    create a folder with timestamp foreach run

--- wallets_collector/main.py
+++ wallets_collector/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import json
 import random
@@ -44,28 +45,32 @@ def get_request_content_scroll(url, query):
 def getAllWallets(file):
     tqdm.write(f"Reading {file}")
 
-    def getFilesWithoutExtension(parent):
-        files = os.listdir(parent)
+    def getIgnoredWallets():
+        ignoredWallets = []
 
-        return [os.path.splitext(
-            filename)[0] for filename in files]
+        for _, _, files in os.walk(WALLETS_FOLDER):
+            for file in files:
+                filename = os.path.splittext(file)[0]
+                ignoredWallets.append(filename)
+
+        return ignoredWallets
 
     with open(file, "r") as accountsFile:
-        ignoredAccounts = getFilesWithoutExtension(WALLETS_FOLDER)
+        ignoredWallets = getIgnoredWallets()
 
-        accounts = accountsFile.read().splitlines()
-        random.shuffle(accounts)
+        wallets = accountsFile.read().splitlines()
+        random.shuffle(wallets)
 
-        print("Found {0}/{1} ({2}%) accounts already processed.".format(
-            len(ignoredAccounts), len(accounts), round(len(ignoredAccounts) / len(accounts) * 100)))
+        print("Found {0}/{1} ({2}%) wallets already processed.".format(
+            len(ignoredWallets), len(wallets), round(len(ignoredWallets) / len(wallets) * 100)))
 
-        accounts = filter(lambda a: not a.startswith(
-            "erd1qqq") and not a in ignoredAccounts, accounts)
+        wallets = filter(lambda a: not a.startswith(
+            "erd1qqq") and not a in ignoredWallets, wallets)
 
-        return accounts
+        return wallets
 
 
-def processWallet(wallet):
+def processWallet(parentFolder, wallet):
 
     url = "https://index.multiversx.com/transactions/_search?scroll=1m&size=10000"
     query = {"query": {"bool": {"should": [{"match": {e: wallet}} for e in [
@@ -119,26 +124,30 @@ def processWallet(wallet):
             data[i]["_source"].setdefault("events", []).extend(log)
 
     # Save the fetched and combined data for each wallet to a separate JSON file
-    with open(os.path.join(WALLETS_FOLDER, wallet+".json"), "w") as f:
+    with open(os.path.join(parentFolder, wallet+".json"), "w") as f:
         json.dump(data, f, ensure_ascii=False)
 
 
 def main():
-    # Create a 'wallets' directory if it does not exist
-    os.makedirs(WALLETS_FOLDER, exist_ok=True)
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--list", help="The wallets list",
                         type=str, default=r"lists\all_wallets.txt")
     args = parser.parse_args()
     pbar = tqdm(unit=" account")
 
+    timestamp = str(int(
+        datetime.timestamp(datetime.now())))
+    parentFolder = os.path.join(WALLETS_FOLDER, timestamp)
+
+    os.makedirs(parentFolder, exist_ok=True)
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
 
         futures = []
 
         for wallet in getAllWallets(args.list):
-            futures.append(executor.submit(processWallet, wallet))
+            futures.append(executor.submit(
+                processWallet, parentFolder, wallet))
 
         for __ in concurrent.futures.as_completed(futures):
             pbar.update()
